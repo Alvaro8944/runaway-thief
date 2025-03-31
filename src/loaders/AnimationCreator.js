d77eb937db017e8ed0430e95c02a091b41d44918
Alvaro8944 <aferre18@ucm.es>
2025-03-31 18:22:53 +0200

    crea assetLoader y animationCreator para simplificar boot

---
+++ src/loaders/AnimationCreator.js
@@ -0,0 +1,222 @@
+ /**
+ * Crea todas las animaciones del juego
+ * @param {Phaser.Scene} scene - La escena donde se crearán las animaciones
+ */
+export function createAllAnimations(scene) {
+  createPlayerAnimations(scene);
+  createEnemyAnimations(scene);
+  createEnemy2Animations(scene);
+  createWeaponAnimations(scene);
+}
+
+/**
+ * Crea las animaciones del jugador
+ * @param {Phaser.Scene} scene - La escena donde se crearán las animaciones
+ */
+export function createPlayerAnimations(scene) {
+  // Run
+  scene.anims.create({
+    key: 'run',
+    frames: scene.anims.generateFrameNumbers('player_run', { start: 0, end: 5 }),
+    frameRate: 10,
+    repeat: -1
+  });
+
+  // Idle
+  scene.anims.create({
+    key: 'idle',
+    frames: scene.anims.generateFrameNumbers('player_idle', { start: 0, end: 3 }),
+    frameRate: 10
+  });
+
+  scene.anims.create({
+    key: 'jump',
+    frames: scene.anims.generateFrameNumbers('player_jump', { start: 0, end: 3 }),
+    frameRate: 3
+  });
+
+  scene.anims.create({
+    key: 'crawl',
+    frames: [{ key: 'player_jump', frame: 3 }],
+    frameRate: 10
+  });
+
+  // ---- SHOOT BODY ----
+  scene.anims.create({
+    key: 'run_shoot',
+    frames: scene.anims.generateFrameNumbers('player_run_shoot', { start: 0, end: 5 }),
+    frameRate: 10,
+    repeat: -1
+  });
+
+  scene.anims.create({
+    key: 'idle_shoot',
+    frames: scene.anims.generateFrameNumbers('player_idle_shoot', { start: 0, end: 3 }),
+    frameRate: 10
+  });
+
+  scene.anims.create({
+    key: 'sit_shoot',
+    frames: [{ key: 'player_sit_shoot', frame: 2 }],
+    frameRate: 10,
+    repeat: 0 // 🔹 La animación se ejecuta solo una vez
+  });
+
+  scene.anims.create({
+    key: 'jump_shoot',
+    frames: scene.anims.generateFrameNumbers('player_jump_shoot', { start: 0, end: 3 }),
+    frameRate: 3
+  });
+
+  // Animación de daño
+  scene.anims.create({
+    key: 'player_hurt',
+    frames: scene.anims.generateFrameNumbers('player_hurt', { start: 0, end: 1 }),
+    frameRate: 10,
+    repeat: 0
+  });
+
+  // Animación de muerte
+  scene.anims.create({
+    key: 'player_death',
+    frames: scene.anims.generateFrameNumbers('player_death', { start: 0, end: 5 }),
+    frameRate: 10,
+    repeat: 0
+  });
+
+  // Double Jump
+  scene.anims.create({
+    key: 'doublejump',
+    frames: scene.anims.generateFrameNumbers('player_doublejump', { start: 0, end: 5 }),
+    frameRate: 10,
+    repeat: 0
+  });
+
+  // Animación de escalada
+  scene.anims.create({
+    key: 'climb',
+    frames: scene.anims.generateFrameNumbers('player_climb', { start: 0, end: 5 }),
+    frameRate: 10,
+    repeat: -1
+  });
+}
+
+/**
+ * Crea las animaciones de los enemigos tipo 1
+ * @param {Phaser.Scene} scene - La escena donde se crearán las animaciones
+ */
+export function createEnemyAnimations(scene) {
+  scene.anims.create({
+    key: 'enemy1_idle',
+    frames: scene.anims.generateFrameNumbers('enemy1_idle', { start: 0, end: 3 }),
+    frameRate: 10,
+    repeat: -1
+  });
+
+  scene.anims.create({
+    key: 'enemy1_walk',
+    frames: scene.anims.generateFrameNumbers('enemy1_walk', { start: 0, end: 5 }),
+    frameRate: 10,
+    repeat: -1
+  });
+
+  scene.anims.create({
+    key: 'enemy1_attack',
+    frames: scene.anims.generateFrameNumbers('enemy1_attack', { start: 0, end: 5 }),
+    frameRate: 10,
+    repeat: 0
+  });
+  
+  // Animación de recibir daño (hurt)
+  scene.anims.create({
+    key: 'enemy1_hurt',
+    frames: scene.anims.generateFrameNumbers('enemy1_hurt', { start: 0, end: 1 }),
+    frameRate: 10,
+    repeat: 0
+  });
+  
+  // Animación de muerte
+  scene.anims.create({
+    key: 'enemy1_die',
+    frames: scene.anims.generateFrameNumbers('enemy1_die', { start: 0, end: 5 }),
+    frameRate: 10,
+    repeat: 0
+  });
+}
+
+/**
+ * Crea las animaciones de los enemigos tipo 2
+ * @param {Phaser.Scene} scene - La escena donde se crearán las animaciones
+ */
+export function createEnemy2Animations(scene) {
+  scene.anims.create({
+    key: 'enemy2_idle',
+    frames: scene.anims.generateFrameNumbers('enemy2_idle', { start: 0, end: 3 }),
+    frameRate: 10,
+    repeat: -1
+  });
+
+  scene.anims.create({
+    key: 'enemy2_walk',
+    frames: scene.anims.generateFrameNumbers('enemy2_walk', { start: 0, end: 5 }),
+    frameRate: 10,
+    repeat: -1
+  });
+
+  scene.anims.create({
+    key: 'enemy2_attack',
+    frames: scene.anims.generateFrameNumbers('enemy2_attack', { start: 0, end: 5 }),
+    frameRate: 10,
+    repeat: 0
+  });
+  
+  // Animación de recibir daño (hurt)
+  scene.anims.create({
+    key: 'enemy2_hurt',
+    frames: scene.anims.generateFrameNumbers('enemy2_hurt', { start: 0, end: 1 }),
+    frameRate: 10,
+    repeat: 0
+  });
+  
+  // Animación de muerte
+  scene.anims.create({
+    key: 'enemy2_die',
+    frames: scene.anims.generateFrameNumbers('enemy2_die', { start: 0, end: 5 }),
+    frameRate: 10,
+    repeat: 0
+  });
+}
+
+/**
+ * Crea las animaciones de armas y efectos
+ * @param {Phaser.Scene} scene - La escena donde se crearán las animaciones
+ */
+export function createWeaponAnimations(scene) {
+  // Hand
+  scene.anims.create({
+    key: 'hand3',
+    frames: [{ key: 'hand3', frame: 0 }],
+    frameRate: 10
+  });
+
+  // Weapon
+  scene.anims.create({
+    key: 'weapon',
+    frames: [{ key: 'weapon', frame: 0 }],
+    frameRate: 10
+  });
+
+  // Bullet
+  scene.anims.create({
+    key: 'bullet',
+    frames: [{ key: 'bullet', frame: 0 }],
+    frameRate: 10
+  });
+
+  // Effect
+  scene.anims.create({
+    key: 'effect',
+    frames: scene.anims.generateFrameNumbers('effect', { start: 0, end: 5 }),
+    frameRate: 10
+  });
+}
---
+++ src/loaders/AssetLoader.js
@@ -0,0 +1,214 @@
+//PLAYER MOVE
+import player_run from '../../assets/players/main_character/Biker_run.png';
+import player_jump from '../../assets/players/main_character/Biker_jump.png';
+import player_idle from '../../assets/players/main_character/Biker_idle.png';
+import player_hurt from '../../assets/players/main_character/Biker_hurt.png';
+import player_death from '../../assets/players/main_character/Biker_death.png';
+import player_doublejump from '../../assets/players/main_character/Biker_doublejump.png';
+import player_climb from '../../assets/players/main_character/Biker_climb.png';
+
+//PLAYER SHOOT BODY
+import player_run_shoot from '../../assets/players/main_character_shoot_body/Run1.png';
+import player_jump_shoot from '../../assets/players/main_character_shoot_body/Jump1.png';
+import player_idle_shoot from '../../assets/players/main_character_shoot_body/Idle1.png';
+import player_sit_shoot from '../../assets/players/main_character_shoot_body/Sitdown1.png';
+
+//ENEMY 1
+import enemy1_idle from '../../assets/Enemies/1/Idle.png';
+import enemy1_walk from '../../assets/Enemies/1/Walk.png';
+import enemy1_hurt from '../../assets/Enemies/1/Hurt.png';
+import enemy1_attack from '../../assets/Enemies/1/Attack.png';
+import enemy1_die from '../../assets/Enemies/1/Death.png';
+
+//ENEMY 2
+import enemy2_idle from '../../assets/Enemies/2/Idle.png';
+import enemy2_walk from '../../assets/Enemies/2/Walk.png';
+import enemy2_hurt from '../../assets/Enemies/2/Hurt.png';
+import enemy2_attack from '../../assets/Enemies/2/Attack.png';
+import enemy2_die from '../../assets/Enemies/2/Death.png';
+
+//PLAYER SHOOT HAND
+import hand3 from '../../assets/players/main_character_shoot_hands/3.png';
+
+//WEAPON
+import weapon from '../../assets/Guns/4_1.png';
+
+//BULLET
+import bullet from '../../assets/Bullets/5.png';
+
+//SHOOT EFFECT
+import effect from '../../assets/Shoot_effects/9_1.png';
+
+// TILEMAP
+import Tileset from '../../assets/tiled/Tileset.png';
+import Tileset2 from '../../assets/tiled/Tileset2.png';
+import Tileset3 from '../../assets/tiled/Tileset3.png';
+import MainScene from '../../assets/tiled/MainScene.json';
+import Nivel3 from '../../assets/tiled/Nivel3.json';
+import ladder from '../../assets/tiled/Objects/Resized/escalera.png';
+import pichos_arriba from '../../assets/tiled/Objects/Resized/pinchos_grandes_arriba.png';
+import pichos_abajo from '../../assets/tiled/Objects/Resized/pinchos_grandes_abajo.png';
+
+// SOUNDS
+import Disparo from '../../assets/Sounds/Shoot.mp3';
+import LevelSound from '../../assets/Sounds/nivel.mp3';
+import Damage from '../../assets/Sounds/Damage.wav';
+import baseball from '../../assets/Sounds/baseball.wav';
+import Jump from '../../assets/Sounds/jump.flac';
+import Escaleras from '../../assets/Sounds/Escaleras.wav';
+import Shootgun from '../../assets/Sounds/Shootgun.wav';
+
+/**
+ * Carga los sprites del jugador
+ * @param {Phaser.Scene} scene - La escena donde se cargarán los assets
+ */
+export function loadPlayerSprites(scene) {
+  // PLAYER MOVE
+  scene.load.spritesheet('player_idle', player_idle, {
+    frameWidth: 48,
+    frameHeight: 48
+  });
+  scene.load.spritesheet('player_run', player_run, {
+    frameWidth: 48,
+    frameHeight: 48
+  });
+  scene.load.spritesheet('player_jump', player_jump, {
+    frameWidth: 48,
+    frameHeight: 48
+  });
+  scene.load.spritesheet('player_hurt', player_hurt, {
+    frameWidth: 48,
+    frameHeight: 48
+  });
+  scene.load.spritesheet('player_death', player_death, {
+    frameWidth: 48,
+    frameHeight: 48
+  });
+  scene.load.spritesheet('player_doublejump', player_doublejump, {
+    frameWidth: 48,
+    frameHeight: 48
+  });
+  scene.load.spritesheet('player_climb', player_climb, {
+    frameWidth: 48,
+    frameHeight: 48
+  });
+
+  // PLAYER SHOOT BODY
+  scene.load.spritesheet('player_run_shoot', player_run_shoot, {
+    frameWidth: 48,
+    frameHeight: 48
+  });
+  scene.load.spritesheet('player_jump_shoot', player_jump_shoot, {
+    frameWidth: 48,
+    frameHeight: 48
+  });
+  scene.load.spritesheet('player_idle_shoot', player_idle_shoot, {
+    frameWidth: 48,
+    frameHeight: 48
+  });
+  scene.load.spritesheet('player_sit_shoot', player_sit_shoot, {
+    frameWidth: 48,
+    frameHeight: 48
+  });
+}
+
+/**
+ * Carga los sprites de los enemigos
+ * @param {Phaser.Scene} scene - La escena donde se cargarán los assets
+ */
+export function loadEnemySprites(scene) {
+  // ENEMY 1
+  scene.load.spritesheet('enemy1_idle', enemy1_idle, {
+    frameWidth: 48,
+    frameHeight: 48
+  });
+  scene.load.spritesheet('enemy1_walk', enemy1_walk, {
+    frameWidth: 48,
+    frameHeight: 48
+  });
+  scene.load.spritesheet('enemy1_hurt', enemy1_hurt, {
+    frameWidth: 48,
+    frameHeight: 48
+  });
+  scene.load.spritesheet('enemy1_attack', enemy1_attack, {
+    frameWidth: 48,
+    frameHeight: 48
+  });
+  scene.load.spritesheet('enemy1_die', enemy1_die, {
+    frameWidth: 48,
+    frameHeight: 48
+  });
+
+  // ENEMY 2
+  scene.load.spritesheet('enemy2_idle', enemy2_idle, {
+    frameWidth: 48,
+    frameHeight: 48
+  });
+  scene.load.spritesheet('enemy2_walk', enemy2_walk, {
+    frameWidth: 48,
+    frameHeight: 48
+  });
+  scene.load.spritesheet('enemy2_hurt', enemy2_hurt, {
+    frameWidth: 48,
+    frameHeight: 48
+  });
+  scene.load.spritesheet('enemy2_attack', enemy2_attack, {
+    frameWidth: 48,
+    frameHeight: 48
+  });
+  scene.load.spritesheet('enemy2_die', enemy2_die, {
+    frameWidth: 48,
+    frameHeight: 48
+  });
+}
+
+/**
+ * Carga los sprites de armas y efectos
+ * @param {Phaser.Scene} scene - La escena donde se cargarán los assets
+ */
+export function loadWeaponSprites(scene) {
+  scene.load.spritesheet('hand3', hand3, { 
+    frameWidth: 32, 
+    frameHeight: 32 
+  });
+  scene.load.spritesheet('weapon', weapon, { 
+    frameWidth: 29, 
+    frameHeight: 11 
+  });
+  scene.load.spritesheet('bullet', bullet, { 
+    frameWidth: 15, 
+    frameHeight: 4 
+  });
+  scene.load.spritesheet('effect', effect, { 
+    frameWidth: 288, 
+    frameHeight: 48 
+  });
+}
+
+/**
+ * Carga los assets del tilemap
+ * @param {Phaser.Scene} scene - La escena donde se cargarán los assets
+ */
+export function loadTilemapAssets(scene) {
+  scene.load.image('tiles', Tileset);
+  scene.load.image('tiles3', Tileset3);
+  scene.load.image('ladder2', ladder);
+  scene.load.image('pichos_arriba', pichos_arriba);
+  scene.load.image('pichos_abajo', pichos_abajo);
+  scene.load.tilemapTiledJSON('map', MainScene);
+  scene.load.tilemapTiledJSON('map3', Nivel3);
+}
+
+/**
+ * Carga los sonidos del juego
+ * @param {Phaser.Scene} scene - La escena donde se cargarán los assets
+ */
+export function loadSoundAssets(scene) {
+  scene.load.audio('disparo', Disparo);
+  scene.load.audio('level2', LevelSound);
+  scene.load.audio('damage', Damage);
+  scene.load.audio('baseball', baseball);
+  scene.load.audio('jump', Jump);
+  scene.load.audio('escaleras', Escaleras);
+  scene.load.audio('shootgun', Shootgun);
+} 

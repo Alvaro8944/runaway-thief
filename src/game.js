7d954ef9946357826b9d5e908f70ac546f145379
Zack1223 <117909834+Zack1223@users.noreply.github.com>
2025-04-13 22:39:38 +0200

    Game.js actualizado( era necesario para el menu)

--- src/game.js
+++ src/game.js
@@ -2,9 +2,11 @@ import Boot from './scenes/boot.js';
 import Boot2 from './scenes/boot2.js';
 import Boot3 from './scenes/boot3.js';
 import End from './scenes/end.js';
+import HowToPlay from './scenes/HowToPlay.js';
 import Level from './scenes/level.js';
 import Level2 from './scenes/level2.js';
 import Level3 from './scenes/level3.js';
+import MenuScene from './scenes/MenuScene.js';
 import Phaser from 'phaser';
 
 
@@ -22,7 +24,7 @@ let config = {
         autoCenter: Phaser.Scale.CENTER_BOTH
     },
     pixelArt: true,
-    scene: [Boot, Level],
+    scene: [MenuScene,Boot, Level,HowToPlay],
     physics: {
         default: 'arcade',
         arcade: {
